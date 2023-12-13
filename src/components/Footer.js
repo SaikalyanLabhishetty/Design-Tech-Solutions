1ad131676a7b1977ac93f7d049d3b4737acd62cd
Saikalyan Labhishetty <89219380+SaikalyanLabhishetty@users.noreply.github.com>
2023-12-13 13:34:37 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,31 @@
-import React from 'react'
-import "./Footer.css"
+// AppFooter.js
+import React from 'react';
+import './Footer.css';
 
-const Appfooter = () => {
-  return (
-    <>
-    <hr className='hr_1'></hr>
-    <footer>
-        <div class="footer-center">
-        {/* <p className='barcode'>Eccentricpixel</p> */}
-        <p>&copy; 2023 | DESIGN TECH | All Rights Reserved.<br/></p>
-        <p>designtechsolutions2@gmail.com</p>
-        <p>contact: Shiva kumar Pathi - 6281505609, Amar Kumar Nicheng - 7013072159</p>
-        </div>
-    </footer>
-    </>
-  )
+const AppFooter = () => {
+    return (
+        <>
+            <hr className='hr_1'></hr>
+            <footer>
+                <div className="footer-left">
+                    <h5>Contact: </h5>
+                    <p>Managing Director: Tembornikar Rikhitha</p>
+                    <p>Email : designtechsolutions2@gmail.com</p>
+                </div>
+                <div className="footer-center">
+                    <img src='./images/logo_final.png' alt='Small Logo' className='small-logo' />
+                    <h5>&copy; 2023 | DESIGN TECH SOLUTIONS</h5>
+                    <h6>designtechsolutions@designtechsolutionsdts.org.in</h6>
+                    <h6 style={{color:"orange",overflow:"hidden"}}>All Rights Reserved.</h6>
+                </div>
+                <div className="footer-right">
+                    <h5>Additional Contact Details:</h5>
+                    <p>Shiva Kumar Pathi - 6281505609</p>
+                    <p>Amar Kumar Nicheng - 7013072159</p>
+                </div>
+            </footer>
+        </>
+    );
 }
 
-export default Appfooter
+export default AppFooter;
