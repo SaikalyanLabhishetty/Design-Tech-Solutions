aed373ee9069bbd51d04da69c324d36d9d85e87f
Saikalyan Labhishetty <89219380+SaikalyanLabhishetty@users.noreply.github.com>
2023-12-13 13:50:00 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,18 +8,18 @@ const AppFooter = () => {
             <hr className='hr_1'></hr>
             <footer>
                 <div className="footer-left">
-                    <h5>Contact: </h5>
+                    <h5 style={{overflow:"hidden"}}>Contact: </h5>
                     <p>Managing Director: Tembornikar Rikhitha</p>
                     <p>Email : designtechsolutions2@gmail.com</p>
                 </div>
                 <div className="footer-center">
                     <img src='./images/logo_final.png' alt='Small Logo' className='small-logo' />
-                    <h5>&copy; 2023 | DESIGN TECH SOLUTIONS</h5>
-                    <h6>designtechsolutions@designtechsolutionsdts.org.in</h6>
+                    <h5 style={{overflow:"hidden"}}>&copy; 2023 | DESIGN TECH SOLUTIONS</h5>
+                    <h6 style={{overflow:"hidden"}}>designtechsolutions@designtechsolutionsdts.org.in</h6>
                     <h6 style={{color:"orange",overflow:"hidden"}}>All Rights Reserved.</h6>
                 </div>
                 <div className="footer-right">
-                    <h5>Additional Contact Details:</h5>
+                    <h5 style={{overflow:"hidden"}}>Additional Contact Details:</h5>
                     <p>Shiva Kumar Pathi - 6281505609</p>
                     <p>Amar Kumar Nicheng - 7013072159</p>
                 </div>
